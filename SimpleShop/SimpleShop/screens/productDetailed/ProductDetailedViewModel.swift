8773a63b49e9a54a285105982ee578393f7522ad
anton.ihnatovich <anton.ihnatovich>
2019-01-17 10:29:40 +0300

    implemented product detaield view model, added retry button to product list view

---
+++ SimpleShop/SimpleShop/screens/productDetailed/ProductDetailedViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ProductDetailedViewModel.swift
+//  SimpleShop
+//
+//  Created by Anton Ihnatovich on 1/17/19.
+//  Copyright © 2019 Ihnatovich. All rights reserved.
+//
+
+import Foundation
+
+protocol ProductDetailedViewModelProtocol: class {
+    var product: ProductProtocol? { get }
+    
+    var updateUI: ((ProductProtocol?) -> Void)? { get set }
+    var showError: ((Error?) -> Void)? { get set }
+    var presentProgress: ((Bool) -> Void)? { get set }
+    
+    func getDetailedItem()
+}
+
+class ProductDetailedViewModel: ProductDetailedViewModelProtocol {
+    
+    private(set) var product: ProductProtocol? {
+        didSet {
+            updateUI?(product)
+        }
+    }
+    
+    private var id: String
+    
+    var updateUI: ((ProductProtocol?) -> Void)?
+    var showError: ((Error?) -> Void)?
+    var presentProgress: ((Bool) -> Void)?
+    
+    init(with id: String) {
+        self.id = id
+    }
+    
+     func getDetailedItem() {
+        presentProgress?(true)
+        ProductService.shared.productDetailed(for: id, succeed: { [weak self] product in
+            self?.product = product
+            self?.presentProgress?(false)
+            }, errored: { [weak self] error in
+                self?.showError?(error)
+                self?.presentProgress?(false)
+        })
+    }
+}
